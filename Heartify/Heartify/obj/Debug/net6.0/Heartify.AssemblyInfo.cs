4d3da7f601da359b0effa40f32a4376462244ed0
Jordan <dankoneshev@abv.bg>
2024-02-25 09:33:37 +0200

    added main menu component

--- Heartify/Heartify/obj/Debug/net6.0/Heartify.AssemblyInfo.cs
+++ Heartify/Heartify/obj/Debug/net6.0/Heartify.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Heartify")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e36abbc03b7befd2c955d7419eb79c78ba6e95ce")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b20574dbe49a04c6c0a7e614463bd29067f74361")]
 [assembly: System.Reflection.AssemblyProductAttribute("Heartify")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Heartify")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
