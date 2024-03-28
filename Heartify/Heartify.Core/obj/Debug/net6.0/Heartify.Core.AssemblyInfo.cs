bc3c89c5b6bef31031ea52096f3617e3da5e870a
Jordan <dankoneshev@abv.bg>
2024-03-28 22:17:15 +0200

    committed

--- Heartify/Heartify.Core/obj/Debug/net6.0/Heartify.Core.AssemblyInfo.cs
+++ Heartify/Heartify.Core/obj/Debug/net6.0/Heartify.Core.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Heartify.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+302bf101e49c3137894a1c7b03a077667d2242a6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ead13e7ad5a07a9c84d266781565280abb5c565")]
 [assembly: System.Reflection.AssemblyProductAttribute("Heartify.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Heartify.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
