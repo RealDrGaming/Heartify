07c70a5e4fddb4a5eb15e326a26f767847c89d0e
RealDrGaming <114016316+RealDrGaming@users.noreply.github.com>
2024-10-04 12:08:19 +0300

    switched to default connection string

--- Heartify/Heartify/Extensions/ServiceCollectionExtension.cs
+++ Heartify/Heartify/Extensions/ServiceCollectionExtension.cs
@@ -22,7 +22,7 @@ namespace Microsoft.Extensions.DependencyInjection
         }
         public static IServiceCollection AddHeartifyDbContext(this IServiceCollection services, IConfiguration config)
         {
-            var connectionString = config.GetConnectionString("SchoolConnection");
+            var connectionString = config.GetConnectionString("DefaultConnection");
             services.AddDbContext<HeartifyDbContext>(options =>
                 options.UseSqlServer(connectionString));
 
